6fc08a89af96729ce35895d46ab45dfd279aac79
Yuan Zhou <alanazhouyuan@gmail.com>
2021-09-09 15:38:47 -0700

    add Venn Diagram script

---
+++ Venn Diagram.R
@@ -0,0 +1,15 @@
+genes <- paste("gene",1:1000,sep="")
+x <- list(
+  A = sample(genes,300), 
+  B = sample(genes,525), 
+  C = sample(genes,440),
+  D = sample(genes,350)
+)
+
+library(ggvenn)
+ggvenn(
+  x, 
+  fill_color = c("#0073C2FF", "#EFC000FF", "#868686FF", "#CD534CFF"),
+  stroke_size = 0.5, set_name_size = 4
+)
+
