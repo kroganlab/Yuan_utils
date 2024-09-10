3a8e1ebff627716d61554c34ddaab2d8385753b6
Yuan Zhou <alanazhouyuan@gmail.com>
2024-09-10 16:18:46 -0700

    Update FragPipe_to_SAINTexpress.R

--- FragPipe_to_SAINTexpress.R
+++ FragPipe_to_SAINTexpress.R
@@ -63,68 +63,6 @@ artmsQualityControlEvidenceExtended(evidence_file = "/PATH_TO_PROJECT_DIR/eviden
                                     keys_file = "/PATH_TO_PROJECT_DIR/keys.txt", 
                                     plotPCA = FALSE)
 
-# PCA
-# MSstats
-peptideDF <- fread(spectronautPeptideFile)
-peptideDF$PeptideModifiedSequence <- peptideDF$PeptideSequence
-peptideDF$IsotopeLabelType <- "L"
-
-library(MSstats)
-mssquant <- dataProcess(raw = as.data.frame(peptideDF),
-                        MBimpute=F)
-write.table(
-  mssquant$FeatureLevelData,
-  file = 'output/mss-FeatureLevelData.txt',
-  eol = "\n",
-  sep = "\t",
-  quote = FALSE,
-  row.names = FALSE,
-  col.names = TRUE
-)
-outputDirectory <- "/PATH_TO_PROJECT_DIR/output"
-mss.normalized.txt <-file.path(outputDirectory, "mss-FeatureLevelData.txt") 
-normPepInt <- fread (mss.normalized.txt)
-
-normPepInt[, logCenteredIntensity := log2(INTENSITY/(median(INTENSITY, na.rm=TRUE))), by = PEPTIDE]
-normInt.mat <- as.matrix(dcast(normPepInt, PEPTIDE~GROUP+SUBJECT, value.var = "logCenteredIntensity"), rownames = "PEPTIDE")
-
-# subset to complete cases
-normInt.mat <- normInt.mat[complete.cases(normInt.mat),]  # select rows with no missing values
-
-colInfo <- data.table(colname = colnames(normInt.mat))
-# something like, this depends on the structure of your condition names
-# colInfo[,c("treat", "time", "rep") := tstrsplit(colname, "[_.]", keep = c(1,2,5)) ]
-colInfo$treat <- gsub("_.*", "", colInfo$colname)
-# colInfo$rep <- gsub(".*-", "", colInfo$colname)
-colInfo$rep <- gsub(".*_", "", colInfo$colname)
-
-
-title <- NULL
-#PCA
-pcaOut <- prcomp(t(normInt.mat))
-pcaDT <- as.data.table(pcaOut$x, keep.rownames=TRUE)
-
-pcaPercentVar <- round(100 * (pcaOut$sdev^2)/sum(pcaOut$sdev^2), 1)
-
-pcaDT <- merge (pcaDT, colInfo, by.x = "rn", by.y = "colname", all.x = TRUE)
-
-#plot first two components
-p <- ggplot (pcaDT, aes(x=PC1, y=PC2, fill = treat, shape = rep)) + 
-  geom_point(alpha=1.0, size=4) + 
-  ggrepel::geom_text_repel(aes(label=rn), show.legend = FALSE, size = 3) +
-  theme_bw() + 
-  xlab (sprintf ("PC1, %.1f%%", pcaPercentVar[1])) + 
-  ylab (sprintf ("PC2, %.1f%%", pcaPercentVar[2])) + 
-  ggtitle (sprintf ("PCA %s using %d features (log intensity)", title, nrow(normInt.mat))) +
-  scale_color_brewer(type = "qual", palette = "Dark2") +
-  scale_shape_manual(values = 21:25) +
-  #scale_fill_manual(values = c(`05` = "gray", `30` = "black")) +
-  guides(fill = guide_legend(override.aes = list(shape =21) ) ,
-         color = guide_legend(override.aes = list(shape =21) ) )
-# BackupAsPDF(p, "PCA_Complete_Features")
-cairo_pdf(file.path(outputDirectory, "PCA_Complete_Features.pdf"), width = 10, height = 7)
-print (p)
-dev.off()
 
 # if you want to exclude runs, put them in this vector
 # it is the "BioReplicate" column in the keys.txt file
@@ -139,9 +77,12 @@ spc_long <- reshape2::melt(spc, id.vars = c("PROTID", "GENEID", "PROTLEN"))
 spc_long$variable <- as.character(spc_long$variable)
 spc_long$value <- as.integer(spc_long$value)
 spc_long$variable <- gsub("_SPC", "", spc_long$variable)
-spc_long$BioReplicate <- gsub(".*_", "", spc_long$variable)
-spc_long$Condition <- sub('_[^_]*$', '', spc_long$variable)
-spc_long$BioReplicate <- paste(spc_long$Condition, spc_long$BioReplicate, sep = "-")
+# change the last _ to -
+spc_long$BioReplicate <- sub("_(?=[^_]*$)", "-", spc_long$variable, perl=TRUE)
+spc_long$Condition <- gsub("\\-.*", "", spc_long$BioReplicate)
+# spc_long$BioReplicate <- gsub(".*_", "", spc_long$variable)
+# spc_long$Condition <- sub('_[^_]*$', '', spc_long$variable)
+# spc_long$BioReplicate <- paste(spc_long$Condition, spc_long$BioReplicate, sep = "-")
 # check contaminate and remove
 if(length(contaminate) == 1){
   spc_long <- spc_long[-which(spc_long$PROTID == contaminate), ]
